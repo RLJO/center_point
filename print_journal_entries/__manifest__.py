0b7ffb22bef60b19a1ded30b785b8963920968c0
mohamed <mohammedalii80>
2021-03-28 13:25:24 +0200

    [Journal_Entries]

---
+++ print_journal_entries/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Print Journal Entries Report in Odoo',
+    'version': '13.0.0.0',
+    'category': 'Account',
+    'summary': 'Allow to print pdf report of Journal Entries.',
+    'description': """
+    Allow to print pdf report of Journal Entries.
+    journal entry
+    print journal entry 
+    journal entries
+    print journal entry reports
+    account journal entry reports
+    journal reports
+    account entry reports
+
+    
+""",
+    'depends': ['base','account'],
+    'data': [
+            'report/report_journal_entries.xml',
+            'report/report_journal_entries_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'live_test_url':'https://youtu.be/qehLT4WOWPs',
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
