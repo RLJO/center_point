1f78837931fa25f884137459f8f50b63508b2bdd
mohamed <mohammedalii80>
2021-04-01 22:48:20 +0200

    [Vendor_Bill]

--- center_point_bill/__manifest__.py
+++ center_point_bill/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','approvals','sale_management'],
+    'depends': ['base','account_accountant'],
 
     # always loaded
     'data': [
