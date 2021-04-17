56707815be0cf52f54a3856cc22615b3f6be124e
mohamed <mohammedalii80>
2021-04-17 13:35:35 +0200

    [edit_invoive_report]

--- invoice_report/models/models.py
+++ invoice_report/models/models.py
@@ -5,15 +5,6 @@ class NewModule(models.Model):
     _name = 'invoice.report'
 
 
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    def create_sales_order(self):
-        for rec in self:
-            approv = self.env['approval.request'].create({
-                'request_owner_id':rec.partner_id,
-
-            })
 
 
 
